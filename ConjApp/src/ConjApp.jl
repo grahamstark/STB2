d6edc3625d1bdbe251ee81dd869bbb1fc4202636
Graham Stark <graham.stark@virtual-worlds.biz>
2023-06-29 23:05:44 +0100

    remove stb dependency

--- ConjApp/src/ConjApp.jl
+++ ConjApp/src/ConjApp.jl
@@ -98,6 +98,154 @@ function save_output_to_cache(facs::Factors,allo::NamedTuple)
   CACHED_RESULTS[NonVariableFacts(facs)] = allo
 end
 
+function make_default_settings() :: Settings
+  settings = Settings()
+  settings.do_marginal_rates = false
+  settings.requested_threads = 4
+  return settings
+end
+
+
+const DEFAULT_SETTINGS = make_default_settings()
+
+"""
+FIXME this shouldn't be here.
+"""
+function load_system(; scotland = false )::TaxBenefitSystem
+    sys = load_file( joinpath( Definitions.MODEL_PARAMS_DIR, "sys_2022-23.jl"))
+    if ! scotland 
+        load_file!( sys, joinpath( Definitions.MODEL_PARAMS_DIR, "sys_2022-23_ruk.jl"))
+    end
+    weeklyise!( sys )
+    return sys
+end
+
+
+"""
+
+"""
+function map_features!( tb :: TaxBenefitSystem, facs :: Factors )
+  tb.ubi.abolished = false
+  tb.ubi.mt_bens_treatment = ub_as_is # ub_keep_housing
+
+  if facs.level ==
+      "Child - £0; Adult - £63; Pensioner - £190"
+      tb.ubi.adult_amount = 63
+      tb.ubi.child_amount = 0
+      tb.ubi.universal_pension = 190
+  elseif facs.level ==
+      "Child - £41; Adult - £63; Pensioner - £190"
+      tb.ubi.adult_amount = 63
+      tb.ubi.child_amount = 41
+      tb.ubi.universal_pension = 190
+  elseif facs.level ==
+      "Child - £0; Adult - £145; Pensioner - £190"
+      tb.ubi.adult_amount = 145
+      tb.ubi.child_amount = 0
+      tb.ubi.universal_pension = 190
+  elseif facs.level ==
+      "Child - £41; Adult - £145; Pensioner - £190"
+      tb.ubi.adult_amount = 145
+      tb.ubi.child_amount = 41
+      tb.ubi.universal_pension = 190
+  elseif facs.level ==
+      "Child - £63; Adult - £145; Pensioner - £190"
+      tb.ubi.adult_amount = 145
+      tb.ubi.child_amount = 63
+      tb.ubi.universal_pension = 190
+  elseif facs.level ==
+      "Child - £63; Adult - £190; Pensioner - £190"
+      tb.ubi.adult_amount = 190
+      tb.ubi.child_amount = 63
+      tb.ubi.universal_pension = 190
+  elseif facs.level ==
+      "Child - £95; Adult - £190; Pensioner - £230"
+      tb.ubi.adult_amount = 190
+      tb.ubi.child_amount = 95
+      tb.ubi.universal_pension = 230
+  elseif facs.level ==
+      "Child - £41; Adult - £230; Pensioner - £230"
+      tb.ubi.adult_amount = 230
+      tb.ubi.child_amount = 41
+      tb.ubi.universal_pension = 230
+  elseif facs.level ==
+      "Child - £95; Adult - £230; Pensioner - £230"
+      tb.ubi.adult_amount = 230
+      tb.ubi.child_amount = 95
+      tb.ubi.universal_pension = 230
+  else
+      @assert false "non mapped facs.level: $(facs.level)"
+  end 
+
+  if facs.tax == 
+      "Basic rate - 20%; Higher rate - 40%; Additional rate - 45%"
+      tb.it.non_savings_rates = [0.2, 0.4, 0.45 ]
+  elseif facs.tax == 
+      "Basic rate - 23%; Higher rate - 43%; Additional rate - 48%"
+      tb.it.non_savings_rates = [0.23, 0.43, 0.48 ]    
+  elseif facs.tax == 
+      "Basic rate - 30%; Higher rate - 50%; Additional rate - 60%"
+      tb.it.non_savings_rates = [0.3, 0.5, 0.6 ]    
+  elseif facs.tax == 
+      "Basic rate - 40%; Higher rate - 60%; Additional rate - 70%"
+      tb.it.non_savings_rates = [0.4, 0.6, 0.7 ]    
+  elseif facs.tax == 
+      "Basic rate - 48%; Higher rate - 68%; Additional rate - 78%"
+      tb.it.non_savings_rates = [0.48, 0.68, 0.78 ]    
+  elseif facs.tax == 
+      "Basic rate - 50%; Higher rate - 70%; Additional rate - 80%"
+      tb.it.non_savings_rates = [0.5, 0.7, 0.8 ]    
+  elseif facs.tax == 
+      "Basic rate - 65%; Higher rate - 85%; Additional rate - 95%"
+      tb.it.non_savings_rates = [0.65, 0.85, 0.95 ]    
+  else
+      @assert false "non mapped facs.tax: $(facs.tax)"
+  end
+
+  if facs.funding ==
+      "Removal of income tax-free personal allowance"
+      tb.it.personal_allowance = 0.0
+  elseif facs.funding in [
+      "Increased government borrowing",
+      "Corporation tax increase",
+      "Tax for businesses based on carbon emissions",
+      "Tax for individuals based on carbon emissions",
+      "Tax on wealth",
+      "VAT increase"]
+      # TODO nothing yet
+  else
+      @assert false "non mapped facs.funding: $(facs.funding)"
+  end
+  tb.ubi.entitlement = 
+      if facs.eligibility == "People in and out of work are entitled"
+          ub_ent_all
+      elseif facs.eligibility == "Everyone is entitled but people of working age who are not disabled are required to look for work"
+          ub_ent_all_but_non_jobseekers
+      elseif facs.eligibility == "Only people in work are entitled"
+          ub_ent_only_in_work
+      elseif facs.eligibility == "Only people out of work are entitled"
+          ub_ent_only_not_in_work
+      else 
+          @assert false "failed to map |$(facs.eligibility)|"
+      end
+  
+  if facs.means_testing == "People with any or no amount of income are entitled to the full benefit"
+      tb.ubi.income_limit = -1.0
+  elseif facs.means_testing == "Only those with incomes less than £20k are entitled to the full benefit"
+      tb.ubi.income_limit = 20_000.0/WEEKS_PER_YEAR
+  elseif facs.means_testing == "Only those with incomes less than £50k are entitled to the full benefit"
+      tb.ubi.income_limit = 50_000.0/WEEKS_PER_YEAR
+  elseif facs.means_testing == "Only those with incomes less than £125k are entitled to the full benefit"
+      tb.ubi.income_limit = 125_000.0/WEEKS_PER_YEAR
+  else
+      @assert false "failed to map |$(facs.means_testing)|"
+  end
+
+  ## TODO facs.citizenship
+
+  make_ubi_pre_adjustments!( tb )
+end
+
 """
 
 """
@@ -108,7 +256,6 @@ function get_output_from_cache() # removed bacause json doesn't like ::Union{Nam
     @info "getoutput; nvc = " nvc 
     @info "getoutput keys are " keys(CACHED_RESULTS)
     if haskey(CACHED_RESULTS, nvc )
-      # u = riskyhash( DEFAULT_FACTORS )
       @info "got results from CACHED_RESULTS " 
       output = CACHED_RESULTS[nvc]
       return ( response=output_ready, data=output)
@@ -117,16 +264,62 @@ function get_output_from_cache() # removed bacause json doesn't like ::Union{Nam
     return( response=bad_request, data="" )  
 end 
 
+"""
+Specialised run for conjoint model.
 
+"""
+function do_one_conjoint_run!( facs :: Factors, obs :: Observable; settings = DEFAULT_SETTINGS ) :: NamedTuple
+  sys1 = load_system( scotland=false ) 
+  sys2 = deepcopy(sys1)
+  map_features!( sys2, facs )
+  sys = [sys1,sys2]
+  ## , get_system( year=2019, scotland=true )]
+  results = do_one_run( settings, sys, obs )
+  settings.poverty_line = make_poverty_line( results.hh[1], settings )
+  summary = summarise_frames!( results, settings ) 
+
+  outps_pre = create_health_indicator( 
+      results.hh[1], 
+      summary.deciles[1], 
+      settings )
+  outps_post = create_health_indicator( 
+      results.hh[2], 
+      summary.deciles[2], 
+      settings )
+  sf_pre = summarise_sf12( outps_pre, settings )
+  sf_post = summarise_sf12( outps_post, settings )
+
+  facs.mental_health = (sf_post.depressed-sf_pre.depressed)/sf_pre.depressed
+  println( "sf_post.depressed=$(sf_post.depressed); sf_pre.depressed=$(sf_pre.depressed)")
+  facs.poverty = summary.poverty[2].headcount - summary.poverty[1].headcount
+  facs.inequality = summary.inequality[2].gini - summary.inequality[1].gini
+  mainres = (;facs, sys1, sys2, settings, sf_pre, sf_post)
+  preferences = Dict()
+  println( "do_one_conjoint_run! made facs as $facs" )
+  for breakdown in keys(Conjoint.BREAKDOWNS)
+      bvals = Conjoint.BREAKDOWNS[breakdown]
+      for bv in bvals
+          popularity = calc_conjoint_total( bv, facs )
+          default_popularity = calc_conjoint_total( bv, Factors{Float64}() )
+          val = ( ; popularity, default_popularity, summary )
+          preferences[bv] = val
+      end
+  end
+  return (;mainres,preferences)
+end
 
 function make_and_cache_base_results()
-  settings = Conjoint.make_default_settings()
+  settings = make_default_settings()
   obs = Observable( Progress(settings.uuid, "",0,0,0,0))
   tot = 0
   of = on(obs) do p
     tot += p.step
   end
-  results = Conjoint.doonerun!( DEFAULT_FACTORS, obs; settings=settings )  
+  facts = Factors{Float64}()
+  facs = deepcopy( DEFAULT_FACTORS )
+  println( "made facts as $facs")
+  results = do_one_conjoint_run!( facs, obs; settings=settings )  
+  println( "made facts as $facs")
   exres = calc_examples( results.sys1, results.sys2, results.settings )    
   output = results_to_html_conjoint( ( results..., examples=exres  ))  
   save_output_to_cache( DEFAULT_FACTORS, output )
@@ -219,7 +412,7 @@ end
 Execute a run from the queue.
 """
 function dorun( session::Session, facs :: Factors )
-  settings = Conjoint.make_default_settings()  
+  settings = make_default_settings()  
   @info "dorun entered facs are " facs
   obs = Observable( Progress(settings.uuid, "",0,0,0,0))
   completed = 0
