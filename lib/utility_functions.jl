a1fa7df70db7e22db88631f25c783d4bc8d709a2
Graham Stark <graham.stark@northumbria.ac.uk>
2023-12-04 17:46:26 +0000

    utilities

--- lib/utility_functions.jl
+++ lib/utility_functions.jl
@@ -1,8 +1,8 @@
-module Utility
+# TODO module Utility
 
 struct Funcs
     δ :: Number
-    β :: AbstractVector{Number}
+    β :: AbstractVector{<:Number}
     utility :: Function
     expenditure :: Function
     indirect :: Function
@@ -14,97 +14,152 @@ module CES
 
     """
     CES utility u = [ x^δ + y^δ ]^(1/δ) (for 2 goods) for δ != [0,1]
-    δ : δ/(δ-1) is elasticity of substitution.
-    a : shares parameters
+    a : kinda sorta shares parameters
     x : goods
+    NOTE: elasticity of substitution = 1/(1 - δ)
     """
-    function utility( δ :: Number, a::AbstractVector{Number}, x :: AbstractVector{Number})::Number
+    function utility( δ :: Number, a::AbstractVector{<:Number}, x :: AbstractVector{<:Number})::Number
+        @assert sum( a ) ≈ 1
+        @assert length(a) == length(x)
         n=length(x)
         u = 0.0
         for i in 1:n
-            u += a[i]*x[i]^δ
+            u += a[i]*(x[i]^δ)
         end
         u^(1/δ)
     end
 
-    function expenditure( δ :: Number, a::AbstractVector{Number}, u :: Number, p :: AbstractVector{Number} )
+    """
+    Expenditure function - min cost of attaining `u` at prices p with `δ` 
+    """
+    function expenditure( δ :: Number, a::AbstractVector{<:Number}, u :: Number, prices :: AbstractVector{<:Number} )
+        @assert sum( a ) ≈ 1
+        p = prices
         n=length(p)
-        r = δ/(δ-1)
+        @assert length(a) == n
         c = 0.0
         for i in 1:n
-            c += (p[i]/a[i])^r
+            c += (a[i]^δ)*(p[i]^(1-δ))
         end
-        c = c^(1/r)
+        c = c^(1/(1-δ))
         c*u
     end
 
-    function indirect( δ :: Number, a::AbstractVector{Number}, y :: Number, p :: AbstractVector{Number} )
-        n=length(p)
-        r = δ/(δ-1)
-        u = 0.0
-        for i in 1:n
-            u += a[i]*p[i]^r
-        end
-        u = u^(-1/r)
-        y*u
+    """
+    The utility attainable for income y at prices p given `δ` and weights `a`
+    `a`s are kinda sorta shares and should sum to 1. It's common also to set them all to 1.
+    `δ` 
+    """
+    function indirect( δ :: Number, a::AbstractVector{<:Number}, y :: Number, prices :: AbstractVector{<:Number} )
+        u1 = expenditure( δ, a, 1, prices ) # cost of 1 unit of utility
+        return y/u1
     end
 
 
-
-    function marshallian( δ :: Number, a::AbstractVector{Number}, y :: Number, p :: AbstractVector{Number}, which :: Int) :: Number
-        r = δ/(δ-1)
-        v1 = a[which]*p[which]^(r-1)
+    """
+    uncompensated demand for good `which` with prices and income
+    """
+    function marshallian( δ :: Number, a::AbstractVector{<:Number}, income :: Number, prices :: AbstractVector{<:Number}, which :: Int) :: Number
+        y = income
+        p = prices
+        v1 = (a[which]/p[which])^δ
         v2 = 0.0
         n = length(p)
         for i in 1:n
-            v2 += a[i]*p[i]^r
+            v2 += a[i]^δ * p[i]^(1-δ)
         end
         y*v1/v2
     end
 
-    function hicksian( δ :: Number, a::AbstractVector{Number}, u :: Number, p :: AbstractVector{Number}, which :: Int ) :: Number
-        r = δ/(δ-1)
-        v1=a[which]*p[which]^(r-1)
-        v2=0.0
-        n = length(p)
-        for i in 1:n
-            v2 += a[i]*p[i]^r
-        end
-        v2 = v2^((1/r)-1) 
-        v2 * v1 * u
+    """
+    compensated demand for good `which` with `prices` and `utility` 
+    same as the marshallian demand at the income needed for that utility
+    """
+    function hicksian( δ :: Number, a::AbstractVector{<:Number}, utility :: Number, prices :: AbstractVector{<:Number}, which :: Int ) :: Number
+        # 
+        income = expenditure( δ, a, utility, prices )
+        return marshallian( δ, a, income, prices, which )
     end
 
 end # module CES
 
 module CobbDouglas
 
-    function utility( δ :: Vector, x :: Vector ) :: Number
-        u = 0.0
+    function utility( δ :: Number, a :: AbstractVector{<:Number}, x :: AbstractVector{<:Number} ) :: Number
+        @assert δ ≈ 1
         n=length(x)
+        println("n=$n length(a) $(length(a))")
+        @assert length(a) == n 
+        u = 0.0
         for i in 1:n
-            u += x[i]^δ[i]
+            u += x[i]^a[i]
         end
         u
     end
 
-    function marshallian( δ :: Number, y :: Number, p :: AbstractVector{Number}, which :: Int) :: Number
+    """
+    Expenditure function - min cost of attaining `u` at prices p with `δ` 
+    """
+    function expenditure( δ :: Number, a::AbstractVector{<:Number}, utility :: Number, prices :: AbstractVector{<:Number} )
+        @assert sum( a ) ≈ 1
         @assert δ ≈ 1
-
+        p = prices
+        n=length(p)
+        @assert length(a) == n 
+        x = utility
+        for i in 1:n  
+            x *= p[i]^a[i]
+        end
+        x
     end
 
-    function hicksian( δ :: Number, u :: Number, p :: AbstractVector{Number}, which :: Int) :: Number
+    """
+    The utility attainable for income y at prices p given `δ` and weights `a`
+    `a`s are kinda sorta shares and should sum to 1. It's common also to set them all to 1.
+    `δ` 
+    """
+    function indirect( δ :: Number, a::AbstractVector{<:Number}, income :: Number, prices :: AbstractVector{<:Number} )
+        @assert sum( a ) ≈ 1
         @assert δ ≈ 1
+        p = prices
+        n=length(p)
+        t = 1.0
+        for i in 1:n
+            t *= p[i]^a[i]
+        end
+        return income/t
+    end
+
+    function marshallian( δ :: Number, a::AbstractVector{<:Number}, income :: Number, prices :: AbstractVector{<:Number}, which :: Int) :: Number
+        @assert sum( a ) ≈ 1
+        @assert δ ≈ 1
+        return income*a[which]/prices[which]
+    end
+
+    function hicksian( δ :: Number, a::AbstractVector{<:Number}, utility :: Number, prices :: AbstractVector{<:Number}, which :: Int) :: Number
+        @assert sum( a ) ≈ 1
+        @assert δ ≈ 1
+        x = expenditure( δ, a, utility, prices )
+        return marshallian( δ, a,  x, prices, which )
     end
 
 end # CobbDouglas
 
-function icurve( δ :: Number, u :: Number, hicks, start_1=5, end_2=5 ) :: AbstractVector{Number}
+module Leontief
+
+end # Leontief
+
+module Fixed 
+
+end # Fixed
+
+function icurve( δ :: Number, a::AbstractVector{<:Number}, u :: Number, hicks, start_1=5, end_2=5 ) :: AbstractMatrix{<:Number}
     p = ones(2)
     v = zeros(100,2)
     p[2] = 0.1
     for i in 1:100
-        v[i,1] = hicks( δ, u, p, 1 )
-        v[i,2] = hicks( δ, u, p, 2 )
+        v[i,1] = hicks( δ, a, u, p, 1 )
+        v[i,2] = hicks( δ, a, u, p, 2 )
         p[2] += 0.1
     end
     v
@@ -113,17 +168,22 @@ end
 
 
 # start examples
-u55 = CES.utility(0.1,[5,5])
-ic55 = icurve( 0.1, u55, CES.hicksian )
-y55 = CES.expenditure( 0.1, u55, ones(2)) # 10, obvs
+shares = [0.5,0.5]
+prices = ones(2)
+u55 = CES.utility(0.1, shares, [5,5])
+ic55 = icurve( 0.1, shares, u55, CES.hicksian )
+y55 = CES.expenditure( 0.1, shares, u55, prices ) # 10, obvs
+u55i = CES.indirect( 0.1, shares, 10, prices )
+@assert y55 ≈ 10
+println( "u55 $u55 y55 $y55 ic55 $ic55 u55i $u55i")
 
-u44 = CES.utility(0.1,[4,4])
-ic44 = icurve( 0.1, u44, CES.hicksian )
-y44 = CES.expenditure( 0.1, u44, ones(2)) # 10, obvs
+u44 = CES.utility(0.1,shares, [4,4])
+ic44 = icurve( 0.1, shares, u44, CES.hicksian )
+y44 = CES.expenditure( 0.1, shares, u44, prices) # 10, obvs
 
-u33 = CES.utility(0.1,[3,3])
-ic33 = icurve( 0.1, u33, CES.hicksian )
-y33 = CES.expenditure( 0.1, u33, ones(2))
+u33 = CES.utility(0.1,shares, [3,3])
+ic33 = icurve( 0.1, shares, u33, CES.hicksian )
+y33 = CES.expenditure( 0.1, shares, u33, prices )
 
 
 
